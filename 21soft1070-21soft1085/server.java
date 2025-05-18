1933147138d9b643cc00c82f9206c92b9c7ec7ab
AbdelrahmanElmughrabi <abdrahman.mughrabi14@gmail.com>
2025-05-19 00:02:31 +0300

    Fixed typo

--- 21soft1070-21soft1085/server.java
+++ 21soft1070-21soft1085/server.java
@@ -1,48 +1,12 @@
-import java.net.Socket;
 
 public class server {
-    
+
     public static void main(String[] args) {
 
         //establish connection woith client
-
         // recieve the request or data from client
-
         //confirmation send ot the client
-        
-        
     }
-
-
-public static void communicateWithClient(int rqChoice){
-
-}
-
-public static int LbConnect(){
-            Socket lbSocket = new Socket("localhost", 6789);
-
-        //establish connection with LB
-
-        //recieve the request type from LB
-
-        
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 //message format
 //joins the
@@ -51,7 +15,6 @@ public static int LbConnect(){
 //recive or proccess request from client
 
 //The server must recive multiple kinds of requests
-
 //ways of knowing the request kinds:
 // either the LB tells the server about the request
 // detections same as the one may be used in the client
